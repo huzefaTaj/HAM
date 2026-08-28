7b66d41ca41a6f90d02a3b51dbebd6746604458d
huzefaTaj <huzefataj8@gmail.com>
2026-08-28 15:26:19 +0530

    update settings

--- ham/settings.py
+++ ham/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-2+&@&e^sl4%u&ve^+%@4wd(%iy!7geg7d-^&j=ck1g$^o!nj!#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'testserver']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'testserver','*']
 
 
 # Application definition
